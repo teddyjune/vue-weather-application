847671aea7bfda8d36bb81803e2e865dd6f9fae0
unknown <teddyjune@naver.com>
2022-05-12 11:54:53 +0900

    first commit

---
+++ src/components/weather/CitySelector.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <ul class="city-selector">
+      <li
+        @click="selectCity(city)"
+        :class="{ isSelected: city.selected }"
+        v-for="city in cities"
+        :key="city.code"
+      >
+        {{ city.label }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CitySelector",
+  data() {
+    return {
+      cities: [
+        { code: "seoul", label: "서울", selected: false },
+        { code: "porto", label: "포르투", selected: false },
+        { code: "hawaii", label: "하와이", selected: false },
+        { code: "dubrovnik", label: "두브로브니크", selected: false },
+        { code: "lausanne", label: "로잔", selected: false },
+        { code: "capri", label: "카프리", selected: false },
+        { code: "london", label: "런던", selected: false },
+        { code: "bali", label: "발리", selected: false },
+      ],
+    };
+  },
+  methods: {
+    selectCity(city) {
+      city.selected = !city.selected;
+      this.$emit("selectCity", city);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.city-selector {
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+}
+.city-selector li {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 0.05em solid #efefef;
+  padding: 0.3em;
+  cursor: pointer;
+}
+.isSelected {
+  background: #fd1d1d;
+  color: #fff;
+}
+</style>
---
+++ src/components/weather/WeatherContent.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="weather-wrapper">
+    <ContentHeader />
+    <CitySelector @selectCity="selectCity" />
+    <WeatherList :weatherList="weatherList" />
+  </div>
+</template>
+
+<script>
+import weatherMixin from "@/mixins/weatherMixin";
+import ContentHeader from "./ContentHeader";
+import CitySelector from "./CitySelector";
+import WeatherList from "./WeatherList";
+export default {
+  name: "WeatherContent",
+  components: {
+    ContentHeader,
+    CitySelector,
+    WeatherList,
+  },
+  mixins: [weatherMixin],
+  data() {
+    return {
+      weatherList: [],
+    };
+  },
+  methods: {
+    async selectCity(city) {
+      if (city.selected) {
+        const weather = await this.getWeatherInfo(city);
+        this.weatherList.push(weather);
+      } else {
+        const index = this.weatherList.findIndex(
+          (weather) => weather.code === city.code
+        );
+        this.weatherList.splice(index, 1);
+      }
+    },
+  },
+};
+</script>
+
+<style></style>
